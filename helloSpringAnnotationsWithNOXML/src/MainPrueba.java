45b1c872322ad11360fda953c7f82c3fc975ec37
A153655 <A153655@PCS32859.grupo.cm.es>
2012-08-22 09:28:47 +0200

    comentarios

--- helloSpringAnnotationsWithNOXML/src/MainPrueba.java
+++ helloSpringAnnotationsWithNOXML/src/MainPrueba.java
@@ -1,11 +1,16 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import ejemplos.spring.ioc.beans.ServicioRemoto;
 
+/**
+ * 
+ * @author A153655
+ *
+ */
 public class MainPrueba {
 
 	public static void main(String[] args) {
+		
 		//ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ServicioRemoto.class);
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext("ejemplos.spring.ioc.beans");
 		ServicioRemoto servicio = applicationContext.getBean("servicioRemoto", ServicioRemoto.class);
