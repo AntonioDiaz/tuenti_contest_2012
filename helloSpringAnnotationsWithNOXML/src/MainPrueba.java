e94e58fa52f1d470cdb22e40b9efffa4d993d3a3
A153655 <A153655@PCS32859.grupo.cm.es>
2012-08-22 09:22:57 +0200

    Dependence Injection example with annotations.

---
+++ helloSpringAnnotationsWithNOXML/src/MainPrueba.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ejemplos.spring.ioc.beans.ServicioRemoto;
+
+public class MainPrueba {
+
+	public static void main(String[] args) {
+		//ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ServicioRemoto.class);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext("ejemplos.spring.ioc.beans");
+		ServicioRemoto servicio = applicationContext.getBean("servicioRemoto", ServicioRemoto.class);
+		System.out.println("El valor es " + servicio.consultaDato());
+	}
+}
